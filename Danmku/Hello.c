488925643cff41a6f192c4867d1b97e89a10daa0
Aipra <1696404585@qq.com>
2020-12-09 19:00:00 +0800

    Hello World

---
+++ Danmku/Hello.c
@@ -0,0 +1,18 @@
+/**
+ * @file Hello.c
+ * @author ChiHeShui
+ * @brief CHS
+ * @version 0.0.0.1
+ * @date 2020-12-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+#include <stdio.h>
+int main (void)
+{
+    printf("Hello world!");
+    return 0;
+}
